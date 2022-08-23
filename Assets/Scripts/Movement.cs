a0857fe03d346cdd915514f75f4f1c872bbe66b7
Elnaz-adv <elnaz.kh90@gmail.com>
2022-08-23 12:26:12 +0200

    collision is added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
     [SerializeField] float mainThrust = 100f ;
     [SerializeField] float rotationThrust = 1f ;
     void Start()
     {
        rb = GetComponent<Rigidbody>();
+       audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,8 +26,16 @@ public class Movement : MonoBehaviour
         
         if(Input.GetKey(KeyCode.Space))
         {
-        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            } 
         }
+        else
+        {
+            audioSource.Stop();
+        }   
     
     }
 
